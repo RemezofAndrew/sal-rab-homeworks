816328c2558fc186d2e41ef4ff7320b906c0f0cb
andrew <holzed228@yandex.ru>
2021-08-10 19:39:13 +0300

    gfh

--- task_2/solution.js
+++ task_2/solution.js
@@ -41,7 +41,7 @@ function calcDiscount(sum, min, discount) {
     // иначе присвойте discountSum значение 0
     if (productsSum >= discountMinSum)
     discountSum = discountPart;
-    discountSum = 0;
+    else discountSum = 0;
 
     // Конец решения задания №2.2.
 
