e296af4de37ce1d77237cdd0cc366543c4eab95f
andrew <holzed228@yandex.ru>
2021-08-10 19:56:10 +0300

    sdf

--- task_2/solution.js
+++ task_2/solution.js
@@ -10,7 +10,7 @@ function calcShipping(sum, min, shipping) {
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
     if (productsSum = 0)
-    shippingSum = 0;
+    else (shippingSum = 0);
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
     if (productsSum >= freeShippingMinSum)
