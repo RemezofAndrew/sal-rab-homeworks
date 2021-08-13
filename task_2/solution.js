f32f6f0dca8bb809eaddf33ff548606a18e5c221
andrew <holzed228@yandex.ru>
2021-08-13 14:01:03 +0300

    fgh

--- task_2/solution.js
+++ task_2/solution.js
@@ -10,15 +10,15 @@ function calcShipping(sum, min, shipping) {
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
     if (productsSum == 0) {
-        let shippingSum = 0
+        shippingSum = 0
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
     } else if(productsSum >= freeShippingMinSum) {
-       let shippingSum = 0 
+       shippingSum = 0 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
     } else if(productsSum > 0 && productsSum < freeShippingMinSum) {
-       let shippingSum = shippingPrice } ;
+       shippingSum = shippingPrice } ;
     // Конец решения задания №2.1.
 
     return shippingSum;
