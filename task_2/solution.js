c375dcee280b66b539a2b6bd9a887f34775e8a50
andrew <holzed228@yandex.ru>
2021-08-13 13:32:22 +0300

    dfg

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,16 +9,16 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-    if (productsSum = "0") {
-    } else (let shippingSum = "0") {};
+    if (productsSum = 0) {
+        let shippingSum = 0}  ;
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-    if (productsSum >= freeShippingMinSum) {
-    }else (let shippingSum = "0") {};
+    else if (productsSum >= freeShippingMinSum) {
+        let shippingSum = 0 };
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    if (productsSum >= freeShippingMinSum) {
-    } else (let shippingSum = shippingPrice) {};
+    else if (productsSum >= freeShippingMinSum) {
+        let shippingSum = shippingPrice } ;
     // Конец решения задания №2.1.
 
     return shippingSum;
