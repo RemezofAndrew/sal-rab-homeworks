f6cba34cdbfd0e0f5d4fa87d38eb6506ae4fec90
andrew <holzed228@yandex.ru>
2021-08-14 15:06:54 +0300

    dfg

--- task_3/solution.js
+++ task_3/solution.js
@@ -15,7 +15,8 @@ function renderCartItem(item) {
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
     // 1. Создайте переменную itemCountText, равную пустой строке
-    let itemCountText = ""
+    let itemCountText
+    itemCountText = ""
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     itemCountText = itemCountText + count
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
