84c1a1ac2f4e47f897e1e6406bc7f80745bb83d6
andrew <holzed228@yandex.ru>
2021-08-18 17:46:58 +0300

    fdg

--- task_2/solution.js
+++ task_2/solution.js
@@ -42,7 +42,7 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-    discountSum = productsSum >= discountMinSum ? productsSum * (discountPart / 100) : 0
+    discountSum = productsSum >= discountMinSum ? productsSum * (discountPart / 100) : 0;
 
     
     // Конец решения задания №2.2.
