c8d285f61623fcc49239fdfc3710e228deadf9d1
andrew <holzed228@yandex.ru>
2021-08-21 18:01:43 +0300

    fgh

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,7 +9,7 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-    if (productsSum = 0) {
+    if (productsSum == 0) {
 
         shippingSum = 0
 
