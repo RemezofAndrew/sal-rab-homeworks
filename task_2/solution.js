77df147185f832d974ecbbc919f66b9cb3ec4a93
andrew <holzed228@yandex.ru>
2021-08-24 21:47:21 +0300

    uil

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,10 +16,18 @@ function calcShipping(sum, min, shipping) {
     }
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
+    else if(productsSum >= freeShippingMinSum) {
+
+        shippingSum = 0 
+        
+    }
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
     else if (productsSum > 0 && productsSum < freeShippingMinSum) {
-        shippingSum = shippingPrice }
+        
+        shippingSum = shippingPrice
+    
+    }
     
     // Конец решения задания №2.1.
 
