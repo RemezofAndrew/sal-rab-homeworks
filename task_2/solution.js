a0413c89b0d1c5f65788feb7a0163f63af753157
andrew <holzed228@yandex.ru>
2021-08-24 21:43:44 +0300

    hgfj

--- task_2/solution.js
+++ task_2/solution.js
@@ -18,11 +18,8 @@ function calcShipping(sum, min, shipping) {
     // то shippingSum присвоить значение 0
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-     if (productsSum == 0 || productsSum >= freeShippingMinSum) {
-
-        shippingSum = 0;
-      
-     } 
+     if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice }
     
     // Конец решения задания №2.1.
 
